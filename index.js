e30667ccd64d3a7fd456ad28c47ee181aa645dc8
Chris Birk <cmbirk@gmail.com>
2015-12-18 00:50:01 -0600

    move button and attempt to style.  Styles currently not loading

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
 
-exports.eejsBlock_exportColumn = function (hook_name, args, cb) {
-  args.content = args.content + "<a id='previewpdf'>PREVIEW</a>";
+// exports.eejsBlock_exportColumn = function (hook_name, args, cb) {
+//   //args.content = args.content + "<a id='previewpdf'>PREVIEW</a>";
+//   args.content = args.content + eejs.require("ep_print_preview_pdf/templates/editbarButton.ejs");
+//   return cb();
+// }
+
+exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb) {
+  args.content = eejs.require("ep_print_preview_pdf/templates/editbarButton.ejs") + args.content;
   return cb();
 }
--- static/js/index.js
+++ static/js/index.js
@@ -3,11 +3,16 @@ var $ = require('ep_etherpad-lite/static/js/rjquery').$;
 var _ = require('ep_etherpad-lite/static/js/underscore');
 var padcookie = require('ep_etherpad-lite/static/js/pad_cookie').padcookie;
 
+exports.aceEditorCSS = function (hook, cb) {
+  return ["/ep_print_preview_pdf/static/css/preview.css"];
+};
+
 exports.postAceInit = function(hook, context){
   var $outerIframeContents = $('iframe[name="ace_outer"]').contents();
   var $innerIframe = $outerIframeContents.find('iframe');
   var $innerdocbody = $innerIframe.contents().find("#innerdocbody");
   var pdfURL = $("#exportpdfa").attr("href");
+  console.log(pdfURL);
   $('body').append("<div id='pdfWrapper' style='display:none;text-align:center;position:fixed;top:40px;left:0;right:0;bottom:0;z-index:999999'></div>")
   $('#pdfWrapper').append("<div id='pdfpreview' style='position:relative;width:80%;height:100%;margin:auto;'></div>");
 
